ed009ea8351c17b2193d516ae5a5e6f4eb473cea
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2024-03-26 20:20:50 +0100

    fix main image again

--- lib/features/item/widgets/item_main_image.dart
+++ lib/features/item/widgets/item_main_image.dart
@@ -9,12 +9,12 @@ class ItemMainImage extends StatelessWidget {
   final double height;
 
   const ItemMainImage(
-      {required this.item, this.width = 75, this.height = 75, super.key});
+      {required this.item, this.width = 100, this.height = 100, super.key});
 
   @override
   Widget build(BuildContext context) {
     if (item.mainImage?.url == null) {
-      return ItemPlaceholderImage(width: width, height: height);
+      return const ItemPlaceholderImage();
     }
 
     return FadeInImage.memoryNetwork(
@@ -24,7 +24,7 @@ class ItemMainImage extends StatelessWidget {
       height: height,
       fit: BoxFit.cover,
       imageErrorBuilder: (context, error, stackTrace) =>
-          ItemPlaceholderImage(width: width, height: height),
+          const ItemPlaceholderImage(),
     );
   }
 }
