31084b94213c5fed1d6c005c2012974343f62cc9
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2024-03-27 18:29:34 +0100

    add default loan_filter.dart page size

--- lib/features/loan/filters/loan_filter.dart
+++ lib/features/loan/filters/loan_filter.dart
@@ -5,10 +5,13 @@ class LoanFilter {
 
   String? ownerId;
 
+  int? pageSize;
+
   LoanFilter({
     this.search,
     this.tenantId,
     this.ownerId,
+    this.pageSize = 10,
   });
 
   Map<String, String?> toMap() {
@@ -16,6 +19,7 @@ class LoanFilter {
       if (search != null) "search": search,
       if (ownerId != null) "ownerId": ownerId.toString(),
       if (tenantId != null) "tenantId": tenantId.toString(),
+      if (pageSize != null) "pageSize": pageSize.toString(),
     };
   }
 }
